fa4542212f3f9bb501e4444c07778c429d7325a5
ScepticalRabbit <thescepticalrabbit@gmail.com>
2023-07-07 18:00:07 +0100

    Project setup, added python files

---
+++ mlfs/knn/knn.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+TODO
+"""
+
+class KNN:
+    def __init__(self):
+        print('KNN Init')
---
+++ mlfs/test-knn.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+TODO
+"""
+
+import numpy as np
+import knn.knn
+
+
+testObj = knn.knn.KNN()
