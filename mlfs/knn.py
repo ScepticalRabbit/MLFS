c7d4abdb74e5958f2ecb47e89ca935d4ace53b3c
ScepticalRabbit <thescepticalrabbit@gmail.com>
2023-07-08 14:49:14 +0100

    Finished KNN tutorial code

--- mlfs/knn.py
+++ mlfs/knn.py
@@ -1,10 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-TODO
+===============================================================================
+MLFS: KNN Algorithm
+===============================================================================
+
+Link to video tutorial
+https://www.youtube.com/watch?v=rTEtEy5o3X0&list=PLcWfeUsAys2k_xub3mHks85sBHZvg24Jd&index=2
 """
 
+'''
+Implementing the KNN algorithm
+-------------------------------------------------------------------------------
+1. Give a data point and calculate its distance to all other data points
+2. Get the closest k points
+3. Perform a task with the points:
+    a) Regression: calculate the average of the k points
+    b) Classification: get the majority vote
+
+'''
+
+# Imports
+import numpy as np
+from collections import Counter
+
+# Global helper functions
+def dist_euclid(x1,x2):
+    dist = np.sqrt(np.sum(x1-x2)**2)
+    return dist
+    
+# KNN class def
 class KNN:
-    def __init__(self):
-        print('KNN Init')
+    def __init__(self,k=3):
+        self.k = k
+        
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+    
+    def predict(self, X):
+        self.preds = [self._predict(x) for x in X]
+        return self.preds
+    
+    def _predict(self, x):
+        # Compute distance to all training data
+        dists = [dist_euclid(x, x_t) for x_t in self.X_train]
+        
+        # Get the closest k points
+        k_inds = np.argsort(dists)[:self.k]
+        k_near_labs = [self.y_train[ii] for ii in k_inds]
+        
+        # Find majority vote
+        most_common = Counter(k_near_labs).most_common()
+        return most_common[0][0]
+
+        
         
 
--- scripts/test-knn.py
+++ scripts/test-knn.py
@@ -1,9 +1,48 @@
 # -*- coding: utf-8 -*-
 """
-TODO
+===============================================================================
+MLFS: KNN Algorithm Tester
+===============================================================================
+
+Link to video tutorial
+https://www.youtube.com/watch?v=rTEtEy5o3X0&list=PLcWfeUsAys2k_xub3mHks85sBHZvg24Jd&index=2
 """
 
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 import mlfs
 
-knn = mlfs.KNN()
+cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
+
+print("----------------------------------")
+print("KNN Tester")
+print("----------------------------------")
+
+iris = datasets.load_iris()
+X,y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+                                                    
+# Plot the data
+plt.figure()
+plt.scatter(X[:,2],X[:,3], c=y, cmap=cmap, edgecolor='k', s=20)
+plt.show()
+
+# Create a classifier KNN class
+clf = mlfs.KNN(k=5)
+clf.fit(X_train,y_train)
+preds = clf.predict(X_test)
+print("Predictions on test data:")
+print(preds)
+
+accuracy = np.sum(preds == y_test) / len(y_test)
+print()
+print("Accuracy = {:.4f}".format(accuracy))
+
+print()
+print("Finished.")
+
 
