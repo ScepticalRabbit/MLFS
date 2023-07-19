633bce69935f1fbb159bebeeb6aa630134daa6ca
ScepticalRabbit <thescepticalrabbit@gmail.com>
2023-07-19 10:32:04 +0100

    Added perceptron

---
+++ mlfs/perceptron.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+===============================================================================
+MLFS: ? Algorithm
+===============================================================================
+
+Link to video tutorial:
+
+"""
+import numpy as np
+
+class Temp:
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X, y):
+        pass
